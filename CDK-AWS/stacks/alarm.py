0582e1ed0da50382892d1ee1bf80f366969f8a73
tejanarni <teja@pearlthoughts.com>
2022-12-27 16:11:40 +0530

    synth

--- CDK-AWS/stacks/alarm.py
+++ CDK-AWS/stacks/alarm.py
@@ -1,14 +1,11 @@
-### SNS Topic ###
-
 from aws_cdk import (
+    aws_lambda as lb,
+    aws_events as events,
+    aws_events_targets as targets,
     aws_sns as sns,
+    aws_sns_subscriptions as subs,
     core,
 )
-
-
-#import aws_cdk.aws_sns as sns
-
-# data_protection_policy: Any
 
 
 class AlarmStack(core.Stack):
@@ -16,12 +13,22 @@ class AlarmStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        cfn_topic = sns.CfnTopic(self, "MyCfnTopic",
+            content_based_deduplication=False,
+            data_protection_policy=data_protection_policy,
+            display_name="displayName",
+            fifo_topic=False,
+            kms_master_key_id="kmsMasterKeyId",
+            signature_version="signatureVersion",
+            subscription=[sns.CfnTopic.SubscriptionProperty(
+            endpoint="endpoint",
+            protocol="protocol"
+        )],
+        tags=[CfnTag(
+            key="key",
+            value="value"
+        )],
+        topic_name="topicName"
+        )
 
-topic = sns.Topic(self, "Topic",
-    content_based_deduplication=True,
-    display_name="Customer subscription topic",
-    fifo=True,
-    topic_name="customerTopic"
-)
-
-#my_topic.add_subscription(subscriptions.UrlSubscription("https://foobar.com/"))
+        
