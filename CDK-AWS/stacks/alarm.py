204762d08df9a7e082ed820c0043bdfd3f42beff
tejanarni <teja@pearlthoughts.com>
2022-12-27 16:15:52 +0530

    test tpopic

--- CDK-AWS/stacks/alarm.py
+++ CDK-AWS/stacks/alarm.py
@@ -13,22 +13,11 @@ class AlarmStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        cfn_topic = sns.CfnTopic(self, "MyCfnTopic",
-            content_based_deduplication=False,
-            data_protection_policy=data_protection_policy,
-            display_name="displayName",
-            fifo_topic=False,
-            kms_master_key_id="kmsMasterKeyId",
-            signature_version="signatureVersion",
-            subscription=[sns.CfnTopic.SubscriptionProperty(
-            endpoint="endpoint",
-            protocol="protocol"
-        )],
-        tags=[CfnTag(
-            key="key",
-            value="value"
-        )],
-        topic_name="topicName"
-        )
+        topic = sns.Topic(self, "Topic",
+            content_based_deduplication=True,
+            display_name="Customer subscription topic",
+            fifo=True,
+            topic_name="customerTopic"
+            )
 
         
