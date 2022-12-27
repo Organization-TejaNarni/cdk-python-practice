561d83587d128f6ade2c4a1b90886027ff42451e
tejanarni <teja@pearlthoughts.com>
2022-12-27 16:44:32 +0530

    test alarm

--- CDK-AWS/stacks/alarm.py
+++ CDK-AWS/stacks/alarm.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     aws_events_targets as targets,
     aws_sns as sns,
     aws_sns_subscriptions as subscriptions,
+    aws_cdk.aws_cloudwatch as cloudwatch
     core,
 )
 
@@ -12,13 +13,40 @@ class AlarmStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
+########################  TOPIC Creation ################
         topic = sns.Topic(self, "Topic",
             content_based_deduplication=True,
             display_name="Customer subscription topic",
             fifo=True,
             topic_name="customerTopic"
             )
+
+########################  Subcription to htt[] ################
         topic.add_subscription(subscriptions.UrlSubscription("https://foobar.com/"))
+
+
+######### cloudwatch Alarm ##############33
+
+#####  Using an existing metric #####
+        metric = queue.metric("ApproximateNumberOfMessagesVisible")
+
+
+#########Creating your own metric#######
+# metric = cloudwatch.Metric(
+#     namespace="MyNamespace",
+#     metric_name="MyMetric",
+#     dimensions=dict(MyDimension="MyDimensionValue")
+# )
+
+
+###### Creating the alarm ####
+
+        alarm = cloudwatch.Alarm(self, "Alarm",
+            metric=metric,
+            threshold=100,
+            evaluation_periods=3,
+            datapoints_to_alarm=2
+        )
+
 
         
